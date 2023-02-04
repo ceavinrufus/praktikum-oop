cadd875f538be798afb4da30ff6adc66a69e3dcd
ceavinrufus <18221162@std.stei.itb.ac.id>
2023-02-04 07:43:47 +0700

    200/200

---
+++ Praktikum 0/DuaKali/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        int bilangan;
+        Scanner input = new Scanner(System.in);
+        bilangan = input.nextInt();
+        System.out.println(bilangan*2);
+    }
+}
